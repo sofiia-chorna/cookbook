76c65b0bc2cd11b2af4ef3d79e2fc3d0914a7a7e
Sofiia Chorna <sofiia.chorna@olympe.io>
2022-10-29 06:03:49 +0200

    feat: increase rules

--- shared/src/common/enums/validation/recipe-validation-rule.enum.js
+++ shared/src/common/enums/validation/recipe-validation-rule.enum.js
@@ -2,7 +2,7 @@ const RecipeValidationRule = {
   NAME_MIN_LENGTH: 2,
   NAME_MAX_LENGTH: 30,
   DESCRIPTION_MIN_LENGTH: 2,
-  DESCRIPTION_MAX_LENGTH: 300,
+  DESCRIPTION_MAX_LENGTH: 1000,
 };
 
 export { RecipeValidationRule };
